7a35ae88d15aff8f82d4391ae56eb5fed83eae0a
nyxcodex24 <cristinatorredemer@gmail.com>
2025-11-05 20:45:06 +0100

    fourth commit

--- peek.sh
+++ peek.sh
@@ -1,3 +1,5 @@
 if [[ -z $2 ]]; then lines=3; else lines=$2; fi
-head -n $lines $1; echo "..."; tail -n $lines $1
 
+inputext=$(cat $1 | wc -l)
+
+if [[ $inputext -le $(( 2 * $lines )) ]]; then cat $1; else echo "Warning! Input file is longer than 2 x $lines lines"; head -n $lines $1; echo ...; tail -n $lines $1; fi 
